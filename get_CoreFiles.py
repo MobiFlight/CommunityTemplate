aea80f443aeaeddd5c4138019518a8181788fb82
elral <3263285+elral@users.noreply.github.com>
2026-09-01 06:34:08 +0200

    fix compile error after updating core FW version

--- get_CoreFiles.py
+++ get_CoreFiles.py
@@ -11,8 +11,9 @@ if not os.path.exists(CORESOURCE_DIR):
     print ("Cloning Mobiflight-Firmware repo ... ")
     env.Execute(f'git clone --depth 1 --filter=blob:none --sparse --branch {CORESOURCE_TAG} "{CORESOURCE}" "{CORESOURCE_DIR}"')
     env.Execute(f'git --work-tree="{CORESOURCE_DIR}" --git-dir="{CORESOURCE_DIR}/.git" sparse-checkout set _Boards src')
-    if os.path.isfile(CORESOURCE_DIR + "/platformio.ini"):
-        os.remove(CORESOURCE_DIR + "/platformio.ini")
 else:
     print ("Checking for Mobiflight-Firmware repo updates ... ")
     env.Execute(f'git --work-tree="{CORESOURCE_DIR}" --git-dir="{CORESOURCE_DIR}/.git" pull origin {CORESOURCE_TAG} --depth 100')
+
+if os.path.isfile(CORESOURCE_DIR + "/platformio.ini"):
+    os.remove(CORESOURCE_DIR + "/platformio.ini")
